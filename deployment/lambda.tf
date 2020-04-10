cc2cd2291e0c7c70bba1bfd7dce56fa30d598d10
Yelizaveta <yelisavetatpu@gmail.com>
2020-04-10 21:53:17 +0100

    email service initial implementation

---
+++ deployment/lambda.tf
@@ -0,0 +1,33 @@
+variable "smtp_username" {
+  type = string
+}
+variable "smtp_password" {
+  type = string
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_lambda_permission" "GPCovidResponse-EmailService" {
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.GPCovidResponse-EmailService.function_name}"
+  principal     = "apigateway.amazonaws.com"
+}
+
+resource "aws_lambda_function" "GPCovidResponse-EmailService" {
+  filename      = "../artifact/covid-backend.zip"
+  function_name = "GPCovidResponse-EmailService"
+  role          = "arn:aws:iam::368263227121:role/service-role/GPCovidResponse-Documents-role-k8gh1hnc"
+  handler       = "index.handler"
+  source_code_hash = "${filebase64sha256("../artifact/covid-backend.zip")}"
+  runtime       = "nodejs12.x"
+
+  environment {
+    variables = {
+      SMTP_USERNAME = var.smtp_username,
+      SMTP_PASSWORD = var.smtp_password
+    }
+  }
+
+}
---
+++ index.js
@@ -0,0 +1,74 @@
+"use strict";
+const nodemailer = require("nodemailer");
+
+const sendEmail = (mailOptions) => {
+  var transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.SMTP_USERNAME,
+      pass: process.env.SMTP_PASSWORD,
+    },
+  });
+
+  return new Promise((resolve, reject) => {
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        console.log(`Email sent to: ${email} - ${info.response}`);
+        resolve("Email sent");
+      }
+    });
+  });
+};
+
+exports.handler = async (event) => {
+  return new Promise(async (resolve, reject) => {
+    switch (event.httpMethod) {
+      case "POST":
+        const buff = new Buffer(event.body, "base64");
+        const inputData = buff.toString("ascii");
+
+        const { emailFrom, emailTo, emailSubject, emailBodyHtml } = JSON.parse(
+          inputData
+        );
+
+        sendEmail({
+          from: emailFrom,
+          to: emailTo,
+          subject: emailSubject,
+          html: emailBodyHtml,
+        })
+          .then((res) => {
+            done(null, res);
+          })
+          .catch((err) => {
+            done(err, null);
+          });
+
+        break;
+      default:
+        done(new Error(`Unsupported method "${event.httpMethod}"`));
+    }
+
+    const done = (err, res) => {
+      const response = {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: "",
+      };
+      if (!err) {
+        response.body = JSON.stringify(res);
+        resolve(response);
+      } else {
+        response.body = JSON.stringify(err.message);
+        response.statusCode = 400;
+        reject(response);
+      }
+    };
+  });
+};
